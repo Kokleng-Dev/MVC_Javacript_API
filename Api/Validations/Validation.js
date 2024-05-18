8de20dc821894914bb8fe53f8257cef33ac1f6cd
Kokleng-Dev <y.koklengmedd@gmail.com>
2024-05-19 01:04:11 +0700

    modify version 3

--- Api/Validations/Validation.js
+++ Api/Validations/Validation.js
@@ -1,6 +1,6 @@
 export class Validation {
     
     /*
-        implement your with library or some logic for validation which can use as parent class...
+        implement with library or some logic for validation which can use as parent class...
     */
 }
